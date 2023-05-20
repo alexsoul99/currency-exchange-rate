98bb42e744858a31670e757fefc33718eb4f450b
AlexSoul <almagueralejandro28@gmail.com>
2023-05-20 11:59:43 -0400

    removed the inter font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
-import { Inter } from 'next/font/google'
 import App from '@/components/App'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 	return (
 		<>
-			<main className={`${inter.className}`}>
+			<main>
 				<App />
 			</main>
 		</>
